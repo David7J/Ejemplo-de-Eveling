072d070415b4f914a69aa54f529078192b8b2916
David Jeshua Áreas Pérez <142626297+David7J@users.noreply.github.com>
2024-01-30 11:46:32 -0600

    Add files via upload

---
+++ Ejercicio/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2 { 
+
+public class Class1
+{
+	public Class1()
+	{
+		private int[] cestudiantes = new int[40];
+
+	public Notas() {}
+
+	public Notas(int[] cestudiantes)
+{
+		Cestudiantes = cestudiantes;
+}
+	public void Est(int[] cestudiantes)
+    {
+		this.Cestudiantes = cestudiantes;
+    }
+
+    public int validar(String Mensaje)
+    {
+        int num = 0, v;
+        do
+        {
+            v = 1;
+            try
+            {
+                Console.Write(Mensaje);
+                num = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (Exception error)
+            {
+                v = 0;
+                Console.WriteLine("No se aceptan letras", error.Message);
+            }
+        } while (v == 0);
+        return num;
+    }
+
+  }
+}
---
+++ Ejercicio/Class2.cs
@@ -0,0 +1,14 @@
+﻿using ConsoleApp1;
+
+public class resul{
+
+    public static void main(string[] args)
+    {
+        Class1 ce = new Class1()
+
+
+
+
+
+    }
+}
